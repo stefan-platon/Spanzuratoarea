d2243601540668e9bb75ab16aaa70ebd155f67ae
Stefan Platon <platonfanica@gmail.com>
2018-10-28 14:53:09 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Spanzuratoarea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(36, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start joc !";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(36, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Litere greșite :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cuvânt :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 434);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Greșeli permise :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(207, 434);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1110, 574);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Spanzuratoarea";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Spanzuratoarea
+{
+    public partial class Form1 : Form
+    {
+        Button[] b = new Button [27];
+        Label[] l = new Label[20];
+        Label[] l1 = new Label[20];
+        string[] cuvinte;
+        string cuv; char lit;
+        int z, ok, nr;
+        StreamReader f;
+        int poz;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int i;
+            for (i = 0; i <= 25; i++)
+            {b[i] = new Button();
+            b[i].Width = b[i].Height = 40;
+            b[i].Top = 20;
+            b[i].Left = 35 + i * 40;
+            b[i].Text = Convert.ToString(Convert.ToChar(i + 97));
+            b[i].Font = new Font("Microsoft Sans Serif", 12);
+            b[i].Enabled = false;
+            b[i].Click += new EventHandler(clic);
+            Controls.Add(b[i]);
+        }
+        }
+
+        void citire()
+        {
+            f = File.OpenText("spanzuratoarea.txt");
+            //se citeste vectorul de cuvinte din fisier incapand cu pozitia curenta
+            cuvinte = f.ReadToEnd().Split(new Char[] { ' ' });
+            //se genereaza o pozitie aleatoare in vectorul cuvintelor
+            Random nr = new Random();
+            //i este o pozitie aleatoare intre 0 si nr de cuvinte -1
+            poz = nr.Next(0, cuvinte.Length - 1);
+            //se alege aleatoriu un cuvant
+            cuv = cuvinte[poz];
+        }
+
+        void clic(object sender, EventArgs e)
+        {
+            ok = 1;
+            lit = Convert.ToChar(((Button)sender).Text);
+            for (int i = 0; i < cuv.Length; i++)
+                if (lit == cuv[i])
+                {
+                    l[i].Text = Convert.ToString(lit);
+                    l[i].Font = new Font ("Microsoft Sans Serif", 14);
+                    ok = 0;
+                    nr++;
+                }
+             if ( ok == 1 )
+                {                    
+                    label4.Text = Convert.ToString(6 - z);
+                    l1[z] = new Label();
+                    l1[z].Width = l1[z].Height = 40;
+                    l1[z].Top = 375;
+                    l1[z].Left = 40 + z * 40;
+                    l1[z].Text = Convert.ToString(lit);
+                    l1[z].Font = new Font("Microsoft Sans Serif", 14);
+                    Controls.Add(l1[z]);
+                    z++;
+                    if (z == 1)
+                    {
+                        cap();
+                        label4.Visible = true;
+                        label4.Text = Convert.ToString(6);
+                    }
+                    if (z == 2)
+                        corp();
+                    if (z == 3)
+                        mana_dreapta();
+                    if (z == 4)
+                        mana_stanga();
+                    if (z == 5)
+                        picior_drept();
+                    if (z == 6)
+                        picior_stang();
+                    if (z == 7)
+                    {
+                        for (int j = 0; j <= 25; j++)
+                        {
+                            b[j].Enabled = false;
+                        }
+                        for (int i = 0; i < cuv.Length; i++)
+                            if (l[i].Text == "__")
+                            {
+                                l[i].Text = Convert.ToString(cuv[i]);
+                                l[i].Font = new Font("Microsoft Sans Serif", 14);
+                                l[i].ForeColor = Color.Red;
+                            }
+                        MessageBox.Show(" Nu ați aflat cuvântul la timp ! ");
+                    }
+                }
+            b[lit - 97].Enabled = false;
+            if (nr == cuv.Length && ok == 0 && z < 7)
+            {
+                MessageBox.Show(" Felicitări, ați aflat cuvântul ! ");
+                for (int j = 0; j <= 25; j++)
+                {
+                    b[j].Enabled = false;
+                }
+            }
+        }
+
+        void cap()
+        {
+            System.Drawing.Pen pen1 = new System.Drawing.Pen(System.Drawing.Color.Black, 2);
+            System.Drawing.Graphics graphics = this.CreateGraphics();
+            System.Drawing.Rectangle rectangle = new System.Drawing.Rectangle(750, 150, 100, 100);
+            graphics.DrawEllipse(pen1, rectangle);
+        }
+
+        void corp()
+        {
+            Graphics linie = this.CreateGraphics();
+            Pen penita1 = new Pen(Color.Black, 2);
+            Point p1 = new Point(), p2 = new Point();
+            p1.X = 800;
+            p1.Y = 250;
+            p2.X = 800;
+            p2.Y = 380;
+            linie.DrawLine(penita1, p1, p2); 
+        }
+
+        void mana_dreapta()
+        {
+            Graphics linie = this.CreateGraphics();
+            Pen penita1 = new Pen(Color.Black, 2);
+            Point p1 = new Point(), p2 = new Point();
+            p1.X = 800;
+            p1.Y = 275;
+            p2.X = 890;
+            p2.Y = 350;
+            linie.DrawLine(penita1, p1, p2); 
+        }
+
+        void mana_stanga()
+        {
+            Graphics linie = this.CreateGraphics();
+            Pen penita1 = new Pen(Color.Black, 2);
+            Point p1 = new Point(), p2 = new Point();
+            p1.X = 800;
+            p1.Y = 275;
+            p2.X = 710;
+            p2.Y = 350;
+            linie.DrawLine(penita1, p1, p2); 
+        }
+
+        void picior_stang()
+        {
+            Graphics linie = this.CreateGraphics();
+            Pen penita1 = new Pen(Color.Black, 2);
+            Point p1 = new Point(), p2 = new Point();
+            p1.X = 800;
+            p1.Y = 378;
+            p2.X = 750;
+            p2.Y = 495;
+            linie.DrawLine(penita1, p1, p2); 
+        }
+
+        void picior_drept()
+        {
+            Graphics linie = this.CreateGraphics();
+            Pen penita1 = new Pen(Color.Black, 2);
+            Point p1 = new Point(), p2 = new Point();
+            p1.X = 800;
+            p1.Y = 378;
+            p2.X = 860;
+            p2.Y = 495;
+            linie.DrawLine(penita1, p1, p2); 
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            citire();
+            Graphics linie = this.CreateGraphics();
+            Pen penita1 = new Pen(Color.Black, 5);
+            Point p1 = new Point(), p2 = new Point();
+            p1.X = 800;
+            p1.Y = 100;
+            p2.X = 800;
+            p2.Y = 150;
+            linie.DrawLine(penita1, p1, p2);
+            p1.X = 800;
+            p1.Y = 100;
+            p2.X = 975;
+            p2.Y = 100;
+            linie.DrawLine(penita1, p1, p2);
+            p1.X = 975;
+            p1.Y = 100;
+            p2.X = 975;
+            p2.Y = 550;
+            linie.DrawLine(penita1, p1, p2);
+            p1.X = 630;
+            p1.Y = 550;
+            p2.X = 975;
+            p2.Y = 550;
+            linie.DrawLine(penita1, p1, p2);
+            p1.X = 630;
+            p1.Y = 550;
+            p2.X = 1100;
+            p2.Y = 550;
+            linie.DrawLine(penita1, p1, p2);
+            p1.X = 875;
+            p1.Y = 100;
+            p2.X = 975;
+            p2.Y = 200;
+            linie.DrawLine(penita1, p1, p2);
+            for (int i = 0; i < cuv.Length; i++)
+            {
+                l[i] = new Label();
+                l[i].Width = l[i].Height = 30;
+                l[i].Top = 265;
+                l[i].Left = 35 + i * 35;
+                l[i].Text = "__";
+                l[i].Font = new Font("Microsoft Sans Serif", 12);
+                Controls.Add(l[i]);
+            }
+            for (int j = 0; j <= 25; j++)
+                b[j].Enabled = true;
+            button2.Enabled = true;
+            button1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < cuv.Length; i++)
+            {
+                l[i].Dispose();
+            }
+            for (int j = 0; j < z; j++)
+            {
+                l1[j].Dispose();
+            }
+            z = 0;
+            for (int j = 0; j <= 25; j++)
+            {
+                b[j].Enabled = false;
+            }
+            this.Invalidate();
+            label4.Visible = false;
+            label4.Text = null;
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+    }
+}
